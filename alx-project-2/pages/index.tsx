d013db13a93d8d3adea44f86930226642212e6a7
kalkidan <kalkidanteshale6@gmail.com>
2025-07-24 14:55:45 +0300

    Scaffold a Next.js project with TypeScript and Tailwind CSS

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/layout/Header";
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<Header />
+			<main>
+				<div>
+					<h1>Welcome to Our Home Page</h1>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default Home;
