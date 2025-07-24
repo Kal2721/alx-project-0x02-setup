32c3f8fc700d1e43d25d1f555b9f37269ce6a6c5
kalkidan <kalkidanteshale6@gmail.com>
2025-07-24 14:58:33 +0300

    Scaffold a Next.js project with TypeScript and Tailwind CSS

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import Lind from 'next/link';
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<div>
+				<h3></h3>
+
+				<nav>
+					<ul></ul>
+				</nav>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
