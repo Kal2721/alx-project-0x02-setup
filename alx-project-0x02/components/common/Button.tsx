2aaf012a0f16dc80c300369e8cb30a4e0f5d63e5
kalkidan <kalkidanteshale6@gmail.com>
2025-07-27 10:35:54 +0300

    Creating reusable Button components

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,14 @@
+import { ButtonProps } from '@/interface';
+
+const Button: React.Fc<ButtonProps> = ({
+	size = '',
+	shape = '',
+	...props
+}) => {
+	return (
+		<button className="flex item-center justify-center transition-colors" {...props}>
+		</button>
+	);
+}
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const About: React.FC = () => {
 	return (
 		<div>
 			<Header />
 			<h1>About Us</h1>
+			<Button size= "small" shape="rounded-full" />
 		</div>
 	);
 }
